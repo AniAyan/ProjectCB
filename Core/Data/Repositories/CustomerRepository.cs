9302a823494149bf6fe0307fb8d45aeacfcff471
Ani Atchemyan <ani.atchemyan@org.fcc.am>
2023-12-25 19:13:34 +0400

    com

---
+++ Core/Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public CustomerRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return dbContext.Customers.ToList();
+        }
+    }
+}
